0092b88e24f02d8eb3d2deb901eb89a9755fedc3
marcel-mathias-nolte <marcel-mathias-nolte>
2022-07-18 13:02:00 +0200

    update

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -3,7 +3,7 @@
 $GLOBALS['TL_DCA']['tl_member']['fields']['dsgvo'] = array
 (
     'exclude'                 => true,
-    'eval'                    => array('mandatory'=>true, 'feEditable'=>true, 'feViewable'=>true, 'feGroup'=>'personal', 'tl_class'=>'w50'),
+    'eval'                    => array('mandatory'=>!\defined('TL_MODE') || 'BE' !== TL_MODE, 'feEditable'=>true, 'feViewable'=>true, 'feGroup'=>'personal', 'tl_class'=>'w50'),
     'inputType'               => 'checkbox',
     'sql'                     => "char(1) NOT NULL default ''"
 );
@@ -11,8 +11,8 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['ageApproval'] = array
 (
     'exclude'                 => true,
     'inputType'               => 'fileTree',
-    'eval'                    => array('filesOnly'=>true, 'fieldType'=>'radio', 'mandatory'=>true, 'feEditable'=>true, 'feViewable'=>true, 'feGroup'=>'personal',  'tl_class'=>'clr', 'extensions'=>$GLOBALS['TL_CONFIG']['validImageTypes']),
+    'eval'                    => array('filesOnly'=>true, 'fieldType'=>'radio', 'mandatory'=>!\defined('TL_MODE') || 'BE' !== TL_MODE, 'feEditable'=>true, 'feViewable'=>true, 'feGroup'=>'personal',  'tl_class'=>'clr', 'extensions'=>$GLOBALS['TL_CONFIG']['validImageTypes']),
     'sql'                     => "binary(16) NULL"
 );
-$GLOBALS['TL_DCA']['tl_member']['fields']['email']['eval']['mandatory'] = false;
+//$GLOBALS['TL_DCA']['tl_member']['fields']['email']['eval']['mandatory'] = false;
 $GLOBALS['TL_DCA']['tl_member']['palettes']['default'] .= ';{extended_avs_legend},dsgvo,ageApproval;';
